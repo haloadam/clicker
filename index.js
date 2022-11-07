df279580656b67cd2fb6cb89c709bd610f3fd1b2
zebruh <adam.olah123@gmail.com>
2022-11-07 02:29:37 +0100

    Add picture comparer logic

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const puppeteer = require('puppeteer');
-const readline = require('readline');
+const { PendingXHR } = require('pending-xhr-puppeteer');
+const point = require('./scores');
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require("path");
+const { clearInterval } = require('timers');
+
+/* const readline = require('readline');
 const startTime = new Date().getUTCMilliseconds();
 let finishTime;
 
@@ -30,26 +37,71 @@ function commandLine() {
         start();
         process.exit(0);
     });
+} */
+//TODO
+// input link, input top or bottom, color difference finder algorithm
+
+function delay(ms) {
+    return new Promise(r => setTimeout(r, ms));
 }
 
 async function start() {
-    const browser = await puppeteer.launch();
+    console.log("start");
+    const browser = await puppeteer.launch({ headless: false });
+    if (!fs.existsSync('screenshots')) {
+        fs.mkdirSync('screenshots');
+    }
     const page = await browser.newPage();
-    await page.goto("https://sports.williamhill.com/betting/en-gb/tennis/OB_EV25677424/jordan-hasson-vs-daniel-rodrigues");
+    const pendingXHR = new PendingXHR(page);
+    await page.goto(`https://sports.williamhill.com/betting/en-gb/tennis/OB_EV25769947/panna-udvardy-vs-timea-babos`);
+    // Here all xhr requests are not finished
+    await pendingXHR.waitForAllXhrFinished();
 
-    await page.waitForSelector("#scoreboard_frame");
-    await page.setViewport({ width: 810, height: 415 })
-    await page.screenshot({
-        path: "score.png",
-        clip: {
-            x: 380,
-            y: 170,
-            width: 90,
-            height: 50
+
+    let previousUuid = undefined;
+    let interval = await setInterval(async () => {
+        let currentUuid = crypto.randomUUID();
+        await page.screenshot({ path: `screenshots/${currentUuid}.png`, clip: point.bottom }).catch(e => console.error(e))
+        console.log("screenshot taken with " + currentUuid);
+
+        if (previousUuid && !compareImages(`screenshots/${previousUuid}.png`, `screenshots/${currentUuid}.png`)) {
+            await clearInterval(interval);
+            console.log('clicked')
+            await fs.readdir('screenshots').then((f) => Promise.all(f.map(e => fs.unlink(e))))
+            console.log('cleanup finished')
+
         }
-    });
-
-    while (true)
-        await browser.close();
+        previousUuid = currentUuid;
+    }, 20);
 }
-commandLine();
+
+function compareImages(imageOnePath, imageTwoPath) {
+    if (!imageOnePath || !imageTwoPath)
+        throw new Error('Image does not exist');
+    if (imageOnePath === imageTwoPath)
+        return true;
+    var stats = fs.statSync(imageOnePath);
+    var imageOneSize = stats.size;
+    stats = fs.statSync(imageTwoPath);
+    var imageTwoSize = stats.size;
+    imageOneBytes = getByteArray(imageOnePath);
+    imageTwoBytes = getByteArray(imageTwoPath);
+    if (imageOneSize === imageTwoSize)
+        for (let i = 0; i < imageOneSize; ++i) {
+            if (imageOneBytes[i] !== imageTwoBytes[i])
+                return false;
+        }
+    else
+        return false;
+    return true;
+}
+
+function getByteArray(filePath) {
+    let fileData = fs.readFileSync(filePath).toString('hex');
+    let result = []
+    for (var i = 0; i < fileData.length; i += 2)
+        result.push('0x' + fileData[i] + '' + fileData[i + 1])
+    return result;
+}
+
+start();
---
+++ scores.js
@@ -0,0 +1,16 @@
+const point = {
+    top: {
+        x: 413,
+        y: 172,
+        width: 30,
+        height: 15
+    },
+    bottom: {
+        x: 420,
+        y: 200,
+        width: 30,
+        height: 15
+    }
+};
+
+module.exports = point;
